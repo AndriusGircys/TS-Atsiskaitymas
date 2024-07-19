0202b3ff3724c4317c0a29ca9dd23a06b38b53fb
Andrius Gircys <gircys.a@gmail.com>
2024-07-19 10:56:45 +0300

    antra uzduotis

---
+++ Task 2/script.js
@@ -0,0 +1,34 @@
+var one = {
+    vardas: 'Andrius',
+    pavarde: 'Gircys',
+    amzius: 33
+};
+var two = {
+    vardas: 'Laura',
+    pavarde: 'Girciene',
+    amzius: 30,
+    ugis: 180,
+    svoris: 60
+};
+two = {
+    vardas: 'Elonas',
+    pavarde: 'Gircys',
+    amzius: 4,
+    ugis: 150,
+    svoris: 32
+};
+var three = {
+    vardas: 'Antanas',
+    pavarde: 'Glokas',
+    lytis: 'vyras'
+};
+three = {
+    vardas: 'Onute',
+    pavarde: 'Walteriene',
+    lytis: 'moteris'
+};
+three = {
+    vardas: 'Simas',
+    pavarde: 'Wesonas',
+    lytis: 'sraigtasparnis' // error
+};
--- Task 2/script.ts
+++ Task 2/script.ts
@@ -1,44 +1,57 @@
 /* ------------------------------ TASK 2 -----------------------------------
-Aprašykite type'us, kad kintamieji, kuriems jie priskirti būtų teisingai aprašyti ir visas kodas veiktų teisingai.
-Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.-------------------------------------------------------------------------- */
+Aprašykite type'us, kad kintamieji, kuriems jie priskirti būtų teisingai aprašyti ir
+ visas kodas veiktų teisingai.
+Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą.
+ Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.-------------------------------------------------------------------------- */
 type One = {
-
+vardas: string;
+pavarde: string;
+amzius: number;
 }
 let one: One = {
-  vardas: '',
-  pavarde: '',
-  amzius: 0
+  vardas: 'Andrius',
+  pavarde: 'Gircys',
+  amzius: 33
 };
 type Two = {
-
+vardas: string;
+pavarde: string;
+amzius: number;
+ugis: number;
+svoris: number;
 }
 let two: Two = {
-  vardas: '',
-  pavarde: '',
-  amzius: 0
+  vardas: 'Laura',
+  pavarde: 'Girciene',
+  amzius: 30,
+  ugis: 180,
+  svoris: 60
 };
 two = {
-  vardas: '',
-  pavarde: '',
-  amzius: 0,
-  ugis: 0,
-  svoris: 0
+  vardas: 'Elonas',
+  pavarde: 'Gircys',
+  amzius: 4,
+  ugis: 150,
+  svoris: 32
 };
 type Three = {
+vardas: string;
+pavarde: string;
+lytis: string;
 
 }
 let three: Three = {
-  vardas: '',
-  pavarde: '',
+  vardas: 'Antanas',
+  pavarde: 'Glokas',
   lytis: 'vyras'
 };
 three = {
-  vardas: '',
-  pavarde: '',
+  vardas: 'Onute',
+  pavarde: 'Walteriene',
   lytis: 'moteris'
 };
 three = {
-  vardas: '',
-  pavarde: '',
-  lytis: 'sraigtasparnis' // error
+  vardas: 'Simas',
+  pavarde: 'Wesonas',
+  // lytis: 'sraigtasparnis' 
 };
