4e48519cad449badb844f3343b14ec14869899a9
Andrius Gircys <gircys.a@gmail.com>
2024-07-19 11:16:52 +0300

    3 uzdavinys

--- Task 3/script.ts
+++ Task 3/script.ts
@@ -1,33 +1,51 @@
 /* ------------------------------ TASK 3 -----------------------------------
-Perpanaudokite aprašytą type'ą, kad sukurti naujus tipus, kuriuos priskyrus kintamiesiems, visas kodas veiktų teisingai.
-Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
+Perpanaudokite aprašytą type'ą, kad sukurti naujus tipus, kuriuos priskyrus kintamiesiems,
+ visas kodas veiktų teisingai.
+Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą.
+ Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
 
 Pastaba: Aprašyto tipo NEKEISTI
 -------------------------------------------------------------------------- */
 
+
 type TipasNaudoti = {
-  marke: string,
-  modelis: string,
-  metai: number,
-  spalva: string,
-  kilometrazas: number
+  marke: string;
+  modelis: string;
+  metai: number;
+  spalva: string;
+  kilometrazas: number;
 };
 
-const dviratis = {
+// Naujas tipas dviratis: turi tik dalį savybių iš TipasNaudoti
+type Dviratis = Partial<TipasNaudoti> & Pick<TipasNaudoti, 'metai' | 'spalva'>;
+
+// Naujas tipas naujaMasina: atitinka visus TipasNaudoti reikalavimus
+type NaujaMasina = TipasNaudoti;
+
+// Naujas tipas senaMasina: išplėstas TipasNaudoti su papildomomis savybėmis
+type SenaMasina = TipasNaudoti & { surudyjesDugnas: boolean };
+
+// Naudojamas tipas dviratis
+const dviratis: Dviratis = {
   metai: 1999,
-  spalva: ''
+  spalva: 'ruda'
 };
-const naujaMasina = {
-  marke: '',
-  modelis: '',
+
+// Naudojamas tipas naujaMasinatsc 
+const naujaMasina: NaujaMasina = {
+  marke: 'bmw',
+  modelis: '6',
   metai: 2025,
-  spalva: ''
+  spalva: 'juoda',
+  kilometrazas: 565654  // Pridėta reikiama savybė
 };
-const senaMasina = {
-  marke: '',
-  modelis: '',
+
+// Naudojamas tipas senaMasina su papildomomis savybėmis
+const senaMasina: SenaMasina = {
+  marke: 'audi',
+  modelis: 'a6',
   metai: 2025,
-  spalva: '',
+  spalva: 'sidabrine',
   kilometrazas: 999999,
-  surudyjesDugnas: true  
+  surudyjesDugnas: true
 };
