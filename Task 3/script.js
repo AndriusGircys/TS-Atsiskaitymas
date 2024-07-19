6c82a83eeffa4bfaa6be36cb0fe89703c234fa9b
Andrius Gircys <gircys.a@gmail.com>
2024-07-19 11:22:21 +0300

    Pridedu sukompiliavima

---
+++ Task 3/script.js
@@ -0,0 +1,30 @@
+/* ------------------------------ TASK 3 -----------------------------------
+Perpanaudokite aprašytą type'ą, kad sukurti naujus tipus, kuriuos priskyrus kintamiesiems,
+ visas kodas veiktų teisingai.
+Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą.
+ Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
+
+Pastaba: Aprašyto tipo NEKEISTI
+-------------------------------------------------------------------------- */
+// Naudojamas tipas dviratis
+var dviratis = {
+    metai: 1999,
+    spalva: 'ruda'
+};
+// Naudojamas tipas naujaMasinatsc 
+var naujaMasina = {
+    marke: 'bmw',
+    modelis: '6',
+    metai: 2025,
+    spalva: 'juoda',
+    kilometrazas: 565654 // Pridėta reikiama savybė
+};
+// Naudojamas tipas senaMasina su papildomomis savybėmis
+var senaMasina = {
+    marke: 'audi',
+    modelis: 'a6',
+    metai: 2025,
+    spalva: 'sidabrine',
+    kilometrazas: 999999,
+    surudyjesDugnas: true
+};
