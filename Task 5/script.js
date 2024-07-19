397bf1e826c0d645a4814916167574202533d9fc
Andrius Gircys <gircys.a@gmail.com>
2024-07-19 11:30:02 +0300

    5 uzdavinys

--- Task 5/script.js
+++ Task 5/script.js
@@ -7,16 +7,16 @@ Pvz.:
   "scissors", "rock" --> "Player 2 won!"
   "paper", "paper" --> "Draw!"
 -------------------------------------------------------------------------- */
-function playGame(player1: string, player2: string): string {
-  if (player1 === player2) {
-    return "Draw!";
-  } else if (
-    (player1 === "scissors" && player2 === "paper") ||
-    (player1 === "paper" && player2 === "rock") ||
-    (player1 === "rock" && player2 === "scissors")
-  ) {
-    return "Player 1 won!";
-  } else {
-    return "Player 2 won!";
-  }
+function playGame(player1, player2) {
+    if (player1 === player2) {
+        return "Draw!";
+    }
+    else if ((player1 === "scissors" && player2 === "paper") ||
+        (player1 === "paper" && player2 === "rock") ||
+        (player1 === "rock" && player2 === "scissors")) {
+        return "Player 1 won!";
+    }
+    else {
+        return "Player 2 won!";
+    }
 }
